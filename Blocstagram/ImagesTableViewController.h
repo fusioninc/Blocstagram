4611a53aeac912914b65642058bbd73d67eb758a
Jason Kuang <jason.jkuang@gmail.com>
2015-07-02 16:28:00 -0700

    Working Blocstagrm

---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Fusion on 7/2/15.
+//  Copyright (c) 2015 Jason Kuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+
+@end
