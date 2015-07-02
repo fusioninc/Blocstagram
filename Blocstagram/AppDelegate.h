f83ea0b70da1b49d8ecd2be4dac61beb3d74a538
Jason Kuang <jason.jkuang@gmail.com>
2015-07-02 14:20:53 -0700

    Initial Commit

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Fusion on 7/2/15.
+//  Copyright (c) 2015 Jason Kuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Fusion on 7/2/15.
+//  Copyright (c) 2015 Jason Kuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
