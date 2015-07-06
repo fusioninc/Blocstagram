10583c2662e4b9e218e5801cc0c217a81b013441
Jason Kuang <jason.jkuang@gmail.com>
2015-07-05 23:09:14 -0700

    Adding data source from mvc concept

---
+++ Blocstagram/Comment.h
@@ -0,0 +1,20 @@
+//
+//  Comment.h
+//  Blocstagram
+//
+//  Created by Fusion on 7/5/15.
+//  Copyright (c) 2015 Jason Kuang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class User;
+
+@interface Comment : NSObject
+
+@property (nonatomic, strong) NSString *idNumber;
+
+@property (nonatomic, strong) User *from;
+@property (nonatomic, strong) NSString *text;
+
+@end
---
+++ Blocstagram/Comment.m
@@ -0,0 +1,14 @@
+//
+//  Comment.m
+//  Blocstagram
+//
+//  Created by Fusion on 7/5/15.
+//  Copyright (c) 2015 Jason Kuang. All rights reserved.
+//
+
+#import "Comment.h"
+#import "User.h"
+
+@implementation Comment
+
+@end
